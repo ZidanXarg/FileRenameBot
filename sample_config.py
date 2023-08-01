50f3a70d60918b8d2a30b782f7fbeb7f762ac793
Zidan <136644599+ZidanXarg@users.noreply.github.com>
2023-08-01 09:50:22 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "6651602516:AAFUVG9BVgHZSBF2IUQPIPAHONSKArrv7eE"
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = 9583161
+    API_HASH = "aecf9e2b7c655c4f916564ab6d598a73"
     # Update channel for Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # log channel
@@ -13,7 +13,7 @@ class Config(object):
     # Get these values from my.telegram.org
     CHAT_ID = os.environ.get("CHAT_ID", "")
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = 5902612988
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
